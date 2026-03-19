a70f36225d770227f7c78e233114ce8773eca4e4
Sumeet <sumeetattri09@gmail.com>
2026-03-19 13:06:33 +0530

    Add multiple tshirt products to products data

--- src/app/data/products.ts
+++ src/app/data/products.ts
@@ -56,6 +56,132 @@ export const products: Product[] = [
   "featured": false,
   "badge": "SALE"
 },
+{
+  "id": "1773905554605-degghj6",
+  "name": "tshirt",
+  "description": "gym tshirt",
+  "price": 300,
+  "originalPrice": 500,
+  "image": "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/e0502cd7-70ec-4c4c-b687-d1d8e6c4c3e9.jpeg",
+  "images": [
+    "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/e0502cd7-70ec-4c4c-b687-d1d8e6c4c3e9.jpeg"
+  ],
+  "category": "T-Shirts",
+  "sizes": [
+    "S",
+    "M",
+    "L",
+    "XL"
+  ],
+  "inStock": true,
+  "featured": false,
+  "badge": "SALE"
+},
+{
+  "id": "1773905612020-gy8t7l7",
+  "name": "tshirt",
+  "description": "gym tshirt",
+  "price": 300,
+  "originalPrice": 500,
+  "image": "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/045503c6-2f6e-421f-862a-9d1bd1679aca.jpeg",
+  "images": [
+    "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/045503c6-2f6e-421f-862a-9d1bd1679aca.jpeg"
+  ],
+  "category": "T-Shirts",
+  "sizes": [
+    "S",
+    "M",
+    "L",
+    "XL"
+  ],
+  "inStock": true,
+  "featured": false,
+  "badge": "SALE"
+},
+{
+  "id": "1773905638643-gfn9i52",
+  "name": "tshirt",
+  "description": "gym tshirt",
+  "price": 300,
+  "originalPrice": 500,
+  "image": "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/d9f42717-c7ba-4ca8-acce-bbc93d007f12.jpeg",
+  "images": [
+    "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/d9f42717-c7ba-4ca8-acce-bbc93d007f12.jpeg"
+  ],
+  "category": "T-Shirts",
+  "sizes": [
+    "S",
+    "M",
+    "L",
+    "XL"
+  ],
+  "inStock": true,
+  "featured": false,
+  "badge": "SALE"
+},
+{
+  "id": "1773905704640-qc3xfdr",
+  "name": "tshirt",
+  "description": "gym tshirt",
+  "price": 300,
+  "originalPrice": 500,
+  "image": "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/398cdb44-0fd0-458a-9bb2-044741ed0b54.jpeg",
+  "images": [
+    "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/398cdb44-0fd0-458a-9bb2-044741ed0b54.jpeg"
+  ],
+  "category": "T-Shirts",
+  "sizes": [
+    "S",
+    "M",
+    "L",
+    "XL"
+  ],
+  "inStock": true,
+  "featured": false,
+  "badge": "SALE"
+},
+{
+  "id": "1773905734638-fopct92",
+  "name": "tshirt",
+  "description": "gym tshirt",
+  "price": 300,
+  "originalPrice": 500,
+  "image": "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/d08858fd-c8a1-4f07-be4c-98e7dac91284.jpeg",
+  "images": [
+    "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/d08858fd-c8a1-4f07-be4c-98e7dac91284.jpeg"
+  ],
+  "category": "T-Shirts",
+  "sizes": [
+    "S",
+    "M",
+    "L",
+    "XL"
+  ],
+  "inStock": true,
+  "featured": false,
+  "badge": "SALE"
+},
+{
+  "id": "1773905760205-qgqxgai",
+  "name": "tshirt",
+  "description": "gym tshirt",
+  "price": 300,
+  "originalPrice": 500,
+  "image": "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/95e57f60-d26d-446c-8b17-a9d2b0f7c23f.jpeg",
+  "images": [
+    "https://pub-1407f82391df4ab1951418d04be76914.r2.dev/uploads/95e57f60-d26d-446c-8b17-a9d2b0f7c23f.jpeg"
+  ],
+  "category": "T-Shirts",
+  "sizes": [
+    "S",
+    "M",
+    "L",
+    "XL"
+  ],
+  "inStock": true,
+  "featured": false,
+  "badge": "SALE"
+}
 ];
 
 export const categories = [
